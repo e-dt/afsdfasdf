a869cd0759d367832cd4609a3b54476e17ea048f
e-dt <punycodedt@gmail.com>
2022-04-13 20:42:46 +1000

    Delete angelsindustries directory

--- angelsindustries/data-updates.lua
+++
@@ -1,8 +0,0 @@
-local OV = angelsmods.functions.OV
-
-require("prototypes.buildings.nuclear-reactor-updates")
-require("prototypes.recipes.angels-reactor")
-require("prototypes.angels-industries-override")
-require("prototypes.angels-industries-ordening")
-
-OV.execute()
--- angelsindustries/prototypes/overrides/components-base-entity-update/components-base-algae-farms-update.lua
+++
@@ -1,7 +0,0 @@
-if angelsmods.industries.components then
-  local OV = angelsmods.functions.OV
-  -- CUSTOM FIXES FOR BASE GAME ASSEMBLERS (+ ANGELS)
-
-  OV.add_prereq("bio-processing-green", "tech-red-circuit")
-
-end
